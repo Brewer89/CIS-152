c1b7a11b5f0424f1e16b5eae54f4ec7b7a314e6c
Brewer89 <157563542+Brewer89@users.noreply.github.com>
2024-11-07 03:12:09 -0600

    Update CarTest.java

--- DataStructuresFinal/src/tests/CarTest.java
+++ DataStructuresFinal/src/tests/CarTest.java
@@ -16,8 +16,8 @@ package tests;
 * I have not given other fellow student(s) access to my program.
 */
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 import model.Car;
 
 
